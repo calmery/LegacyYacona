b51ccc7225e2b0ee707500b5b077230f654590c4
calmery <g1531046@m.sojo-u.ac.jp>
2017-02-04 20:32:26 +0900

    Add function to get repository index

--- Yacona/remoteInstall.js
+++ Yacona/remoteInstall.js
@@ -1,7 +1,19 @@
+const fs = require( 'fs' )
+
 const utility = require( '../Modules/Utility' )
 
 const simple_git = require( 'simple-git' )
 const repository = utility.fixPath( __dirname, './Repository/' )
+
+const updateRepositoryIndex = () => {
+    let repositoryJson = fs.readFileSync( repository + '/../repository.json', 'utf-8' )
+    repositoryJson = JSON.parse( repositoryJson )
+
+    let index = fs.readdirSync( 'Yacona/Repository/Core/Index' )
+    repositoryJson['Core'] = index
+
+    fs.writeFileSync( repository + '/../repository.json', JSON.stringify( repositoryJson ) )
+}
 
 const tap = ( repo ) => {
     
@@ -20,15 +32,27 @@ const tap = ( repo ) => {
         if( utility.isExist( repository + '/' + name ) === true ){
             let git = simple_git( repository + '/' + name )
             git.pull( ( ...args ) => {
-                if( args[0] === null ) console.log( 'Repository pull completed' )
+                if( args[0] === null ){
+                    
+                    
+                    console.log( 'Repository pull completed' )
+                    
+                    // Repository.json update
+                    // require('fs').readdir( 'Yacona/Repository/Core', ( error, list ) => { console.log( list ) } )
+                    
+                    updateRepositoryIndex()
+                    
+                }
                 else console.log( 'Git (' + repo + ') pull faild.' )
             } )
         } else {
             let git = simple_git( repository )
             git.clone( 'https://github.com/' + path + '.git', name, {}, ( ...args ) => {
                 console.log( args )
-                if( args[0] === null ) console.log( 'Repository clone completed' )
-                else console.log( 'Git (' + repo + ') clone faild.' )
+                if( args[0] === null ){
+                    console.log( 'Repository clone completed' )
+                    updateRepositoryIndex()
+                } else console.log( 'Git (' + repo + ') clone faild.' )
             } )
         }
         
