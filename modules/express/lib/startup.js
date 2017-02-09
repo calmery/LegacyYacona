901b879b0cdf272270b506c7435b51f28b273dc5
calmery <g1531046@m.sojo-u.ac.jp>
2017-02-10 08:33:26 +0900

    Use ejs view engine in express

--- modules/express/lib/startup.js
+++ modules/express/lib/startup.js
@@ -8,14 +8,25 @@ const startup = ( port ) => {
     let app  = express(),
         server = http.Server( app )
     
+    let viewEngine = 'html'
+    
+    if( typeof port !== 'number' ){
+        if( port === 'ejs' ){
+            app.set( 'view engine', 'ejs' )
+            viewEngine = 'ejs'
+        }
+        port = undefined
+    }
+    
     let listen = port === undefined ? server.listen() : server.listen( port )
     
     // Export
     return {
-        server  : server,
-        app     : app,
-        port    : listen.address().port,
-        addRoute: ( path, fn ) => addRoute( app, path, fn )
+        server    : server,
+        app       : app,
+        port      : listen.address().port,
+        viewEngine: viewEngine,
+        addRoute  : ( path, fn ) => addRoute( app, path, fn )
     }
 
 }
