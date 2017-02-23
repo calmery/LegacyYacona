dfc26e96dfef41521d588273862bf6c6980c580d
Marei Kikukawa <contact@calmery.me>
2017-02-24 00:37:48 +0900

    Add auto parse method of yaml

--- modules/yaml/index.js
+++ modules/yaml/index.js
@@ -1,4 +1,4 @@
-const localFunctions = [ 'save', 'load', 'parser' ]
+const localFunctions = [ 'save', 'load', 'parser', 'loader' ]
 
 let fn = {}
 localFunctions.forEach( ( name, index ) => fn[name] = require( './lib/' + name ) )
