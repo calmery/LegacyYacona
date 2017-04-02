70f0dbfe64c0813aeb7f8924e9263d5a2e758a7b
Marei Kikukawa <contact@calmery.me>
2017-04-02 15:18:22 +0900

    Add method to dump json to yaml

--- modules/yaml/index.js
+++ modules/yaml/index.js
@@ -1,4 +1,4 @@
-const localFunctions = [ 'save', 'load', 'parser', 'loader' ]
+const localFunctions = [ 'save', 'load', 'parser', 'loader', 'dump' ]
 
 let fn = {}
 localFunctions.forEach( ( name, index ) => fn[name] = require( './lib/' + name ) )
